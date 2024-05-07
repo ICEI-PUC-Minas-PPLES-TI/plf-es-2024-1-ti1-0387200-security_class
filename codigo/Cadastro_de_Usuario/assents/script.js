89e46c30417323724c543c79ff1ae41035a47a45
matheustitton <matheustitton@gmail.com>
2024-05-07 20:01:21 -0300

    modificações cadastro

--- codigo/Cadastro_de_Usuario/assents/script.js
+++ codigo/Cadastro_de_Usuario/assents/script.js
@@ -1,41 +1,47 @@
 document.getElementById('cadastroForm').addEventListener('submit', function(event) {
     event.preventDefault();
-  
-    // Obter os valores do formulário
+
+    let id = getNextId();
     let nome = document.getElementById('nome').value;
     let sobrenome = document.getElementById('sobrenome').value;
     let nickname = document.getElementById('nickname').value;
+    let idade = document.getElementById('idade').value;
     let celular = document.getElementById('celular').value;
-    let idade = parseInt(document.getElementById('idade').value);
     let sexo = document.getElementById('sexo').value;
     let email = document.getElementById('email').value;
     let senha = document.getElementById('senha').value;
-  
-    // Criar o objeto de usuário
+
     let novoUsuario = {
-      "id": getNextId(), // Função para obter o próximo Id disponível
-      "nome": nome,
-      "sobrenome": sobrenome,
-      "nickname": nickname,
-      "celular": celular,
-      "idade": idade,
-      "sexo": sexo,
-      "email": email,
-      "senha": senha
+        "id": id,
+        "nome": nome,
+        "sobrenome": sobrenome,
+        "nickname": nickname,
+        "idade": idade,
+        "celular": celular,
+        "sexo": sexo,
+        "email": email,
+        "senha": senha
     };
-  
-    // Adicionar o novo usuário ao JSON de usuários
-    let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+    let usuarios = JSON.parse(localStorage.getItem('Cadastro_de_Usuario')) || [];
+    
+    // Verificar se já existe um usuário com o mesmo e-mail
+    let usuarioExistente = usuarios.find(user => user.email === email);
+    if (usuarioExistente) {
+        alert('Já existe um usuário cadastrado com este e-mail.');
+        return;
+    }
+
     usuarios.push(novoUsuario);
-    localStorage.setItem('usuarios', JSON.stringify(usuarios));
-  
+    localStorage.setItem('Cadastro_de_Usuario', JSON.stringify(usuarios));
+
     alert('Usuário cadastrado com sucesso!');
     document.getElementById('cadastroForm').reset();
-  });
-  
-  // Função para obter o próximo Id disponível
-  function getNextId() {
-    let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
-    let nextId = usuarios.length > 0 ? usuarios[usuarios.length - 1].id + 1 : 1;
+});
+
+// Função para obter o próximo ID começando de 0
+function getNextId() {
+    let usuarios = JSON.parse(localStorage.getItem('Cadastro_de_Usuario')) || [];
+    let nextId = usuarios.length;
     return nextId;
-  }
+}
